4277d22eebc2429a87f62accf318202073a64e66
ygrillo <ygrillo@tutanota.com>
2025-05-21 12:00:45 -0300

    feat: Adiciona nova função Atualizar no repositorio de Usuarios

--- src/repositorios/usuarios.go
+++ src/repositorios/usuarios.go
@@ -102,3 +102,17 @@ func (repositorio Usuarios) BuscarPorID(ID uint64) (modelos.Usuario, error) {
 	}
 	return usuario, nil
 }
+
+func (repositorio Usuarios) Atualizar(ID uint64, usuario modelos.Usuario) error {
+	statement, erro := repositorio.db.Prepare("update usuarios set nome = ?, nick = ?, email = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
