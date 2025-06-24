ec4939de6aa071c62fd2511ff09dcd96a2d0387f
ygrillo <ygrillo@tutanota.com>
2025-06-24 12:28:31 -0300

    feat: Implementa a lógica do login

---
+++ src/controllers/login.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"api/src/seguranca"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+// Login é responsável por autenticar um usuário na API
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioSalvoNoBanco, erro := repositorio.BuscarPorEmail(usuario.Email)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = seguranca.VerificarSenha(usuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, errors.New("A senha está incorreta."))
+		return
+	}
+
+	w.Write([]byte("Você está logado! Parabéns!"))
+}
