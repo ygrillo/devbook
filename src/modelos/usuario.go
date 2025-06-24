ad3f575d84717df0e0adcf9a987f102c73ebd93d
ygrillo <ygrillo@tutanota.com>
2025-06-24 11:04:52 -0300

    feat: Implementa validação de e-mail

--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"strings"
 	"time"
+
+	"github.com/badoux/checkmail"
 )
 
 // Usuario representa um usuário utilizando a rede social
@@ -37,6 +39,10 @@ func (usuario *Usuario) validar(etapa string) error {
 
 	if usuario.Email == "" {
 		return errors.New("O e-mail é obrigatório e não pode estar em branco.")
+	}
+
+	if erro := checkmail.ValidateFormat(usuario.Email); erro != nil {
+		return errors.New("O e-mail inserido é inválido.")
 	}
 	
 	if etapa == "cadastro" && usuario.Senha == "" {
